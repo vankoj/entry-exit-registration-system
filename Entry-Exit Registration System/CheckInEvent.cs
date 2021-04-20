63d8e8a73dc512984911a1a18729802370be2768
vankoj <51315090+vankoj@users.noreply.github.com>
2021-04-20 11:59:44 +0300

    Добавен класът CheckInEvent
    Обект от CheckInEvent съхранява събитие за чекиране на служител

---
+++ Entry-Exit Registration System/CheckInEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entry_Exit_Registration_System
+{
+    class CheckInEvent
+    {
+        public string EGN { get; set; }
+        public string F_Name { get; set; }
+        public string L_Name { get; set; }
+        public string Position_Name { get; set; }
+        public DateTime Date_Time { get; set; }
+        public Boolean Is_Entry { get; set; }
+
+        public CheckInEvent(string EGN, string F_Name, string L_Name, string Position_Name, DateTime Date_Time, Boolean Is_Entry)
+        {
+            this.EGN = EGN;
+            this.F_Name = F_Name;
+            this.L_Name = L_Name;
+            this.Position_Name = Position_Name;
+            this.Date_Time = Date_Time;
+            this.Is_Entry = Is_Entry;
+        }
+    }
+}
